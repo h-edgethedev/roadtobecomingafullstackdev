4a8d4dbc0b04df6ae8d9e6a9248e69fc88d9d63d
h-edgethedev <oluwanifemibarber@gmail.com>
2026-05-21 08:25:57 +0100

    learning more and creating structured folders

--- Javascript Basics/DOM manipulation, events/understanding change events.js
+++ Javascript Basics/DOM manipulation, events/understanding change events.js
@@ -2,3 +2,29 @@ const selectMenu = document.getElementById("select-menu");
 selectMenu.addEventListener("change", (event) => {
   console.log(`You selected: ${event.target.value}`);
 });
+
+const p = document.querySelector("p");
+const span = document.querySelector("span");
+// p.addEventListener("click", (event) => {
+//   console.log("P listener: ");
+//   console.log(event.target);
+// });
+// span.addEventListener("click", (event) => {
+//   console.log("Span listener: ");
+//   console.log(event.target);
+// });
+
+// p.addEventListener("click", (event) => {
+//   console.log("P listener: ");
+//   console.log(event.target);
+// });
+// span.addEventListener("click", (event) => {
+//   console.log("Span listener: ");
+//   console.log(event.target);
+//   // event.stopPropagation();
+// });
+
+
+p.addEventListener("click", (event)=>{
+  event.target.style.color= "red"
+})
