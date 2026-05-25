4f9424afc40fe0480861e392da468c9c35434b89
h-edgethedev <oluwanifemibarber@gmail.com>
2026-05-26 00:45:52 +0100

    omoor

--- Javascript Basics/DOM manipulation, events/FreeCodeCamp projects/Musical Instrument filter.js
+++ Javascript Basics/DOM manipulation, events/FreeCodeCamp projects/Musical Instrument filter.js
@@ -1,14 +1,14 @@
-const instrumentsArr= [
+const instrumentsArr = [
     {
-        category : "woodwinds",
-        instrument : "Flute",
+        category: "woodwinds",
+        instrument: "Flute",
         price: 500
     },
     {
         category: "woodwinds",
         instrument: "Clarinet",
         price: 200
-    }, 
+    },
     {
         category: "woodwinds",
         instrument: "Oboe",
@@ -16,17 +16,25 @@ const instrumentsArr= [
     }
 ]
 
-var selectContainer= document.querySelector(".select-container")
-var productsContainer= document.querySelector("products-container")
+var selectContainer = document.querySelector(".select-container")
+var productsContainer = document.querySelector(".products-container")
 
-function instrumentCards(params) {
-    const instruments= params === "all"? instrumentsArr :
-    instrumentsArr.filter(({category}) => category=== params)
-    return instruments.map(({instruments, price}))
-}
+    function instrumentCards(params) {
+        const instruments = params === "all" ? instrumentsArr :
+        instrumentsArr.filter(({ category }) => category === params)
+        
+        return instruments.map(({ instrument, price}) => {
+            return `
+            <div class="card">
+            <h2>${instrument}</h2>
+            <p>$${price}</p>
+        </div>
+            `
+        }).join('')
+    }
 
 
-selectContainer.addEventListener("change", ()=>{
-    console.log(instrumentCards(selectContainer.value))
+selectContainer.addEventListener("change", (event) => {
+    productsContainer.innerHTML= instrumentCards(event.target.value)
 })
 
