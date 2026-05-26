91ba7025c71807e01d7be1c50638e7eed25b2817
h-edgethedev <oluwanifemibarber@gmail.com>
2026-05-26 14:48:34 +0100

    still practicing DOM Events

--- Javascript Basics/DOM manipulation, events/FreeCodeCamp projects/Musical Instrument filter.js
+++ Javascript Basics/DOM manipulation, events/FreeCodeCamp projects/Musical Instrument filter.js
@@ -13,28 +13,63 @@ const instrumentsArr = [
         category: "woodwinds",
         instrument: "Oboe",
         price: 4000
+    },
+    {
+        category: "brass",
+        instrument: "Trumpet",
+        price: 200
+    },
+    {
+        category: "brass",
+        instrument: "Trombone",
+        price: 300
+    },
+    {
+        category: "brass",
+        instrument: "French horn",
+        price: 4300
+    },
+    {
+        category: "percussion",
+        instrument: "Drum Set",
+        price: 500
+    },
+    {
+        category: "percussion",
+        instrument: "Xylophone",
+        price: 3000
+    },
+    {
+        category: "percussion",
+        instrument: "Cymbals",
+        price: 200
+    },
+    {
+        category: "percussion",
+        instrument: "Marimba",
+        price: 3000
     }
 ]
 
 var selectContainer = document.querySelector(".select-container")
 var productsContainer = document.querySelector(".products-container")
 
-    function instrumentCards(params) {
-        const instruments = params === "all" ? instrumentsArr :
+function instrumentCards(params) {
+    const instruments = params === "all" ? instrumentsArr :
         instrumentsArr.filter(({ category }) => category === params)
-        
-        return instruments.map(({ instrument, price}) => {
-            return `
+
+    return instruments.map(({ instrument, price }) => {
+        return `
             <div class="card">
             <h2>${instrument}</h2>
             <p>$${price}</p>
         </div>
             `
-        }).join('')
-    }
+    }).join('')
+}
 
 
 selectContainer.addEventListener("change", (event) => {
-    productsContainer.innerHTML= instrumentCards(event.target.value)
+    productsContainer.innerHTML = instrumentCards(event.target.value)
 })
 
