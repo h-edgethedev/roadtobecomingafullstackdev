3421323cd573953711f78961f8b64b6e9aa2e1c9
h-edgethedev <oluwanifemibarber@gmail.com>
2026-06-14 00:59:42 +0100

    commenced learning regex

--- Javascript Basics/Regex operations in Javascript/script.js
+++ Javascript Basics/Regex operations in Javascript/script.js
@@ -1,2 +1,13 @@
-const regex= "/freeCodeCamp/"
+// const regex= /freeCodeCamp/
+// const match = "I love freeCodeCamp like crazy".match(regex)
+// console.log("FREECODECAMP".match(regex))
+// console.log(match)
 
+const regex= /freecodecamp/
+const str= "freecodecamp is rly kewl"
+var replaced= str.replace(regex, "freeCodeCamp")
+console.log(replaced)
+replaced = replaced.replace(/rly/, "really")
+replaced = replaced.replace(/kewl/, "cool")
+
+console.log(replaced)
